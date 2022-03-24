6e19322b8b3718a172b8cc6a66acfa03b9898957
David Castello <davidcastellobatetgmail.com>
2022-03-24 01:58:03 +0100

    Funcionalitat: Progres video a barra de progres

--- index.js
+++ index.js
@@ -38,6 +38,30 @@ function inicio() {
 
   // evento reiniciar cancion
   document.querySelector(".reiniciar").onclick = reiniciar;
+
+  // evento expandir / contraer video
+  document.querySelector(".expandir").onclick = expandir;
+
+  // actualitzacion continua del tiempo - funcio actualitzarTemps
+  vid.ontimeupdate = actualizarTemps;
+  // actualitzacio del temps quan carreguem el video- actualitza
+  // el temps de ala durada del video i evitara el NaN
+  vid.onloadeddata = actualizarTemps;
+
+  // acceso directo al hacer click en la barra de tiempo
+  document.querySelector(".barra1").onclick = buscar;
+}
+
+// Funciones control de la REPRODUCCION *******************
+// funcion para reordenar los videos del array de forma aleatoria
+function reordenar() {
+  for (video of videos) {
+    do {
+      var azar = Math.floor(Math.random() * videos.length);
+    } while (ordenDeLosVideos.indexOf(azar) >= 0);
+    ordenDeLosVideos.push(azar);
+  }
+  return ordenDeLosVideos;
 }
 
 // funcion para el play / pause
@@ -59,7 +83,9 @@ function siguiente(indice) {
 
 // funcion reiniciar cancion
 let reiniciar = () => (vid.currentTime = 0);
-
+// ************************** fin REPRODUCCION ***********
+//
+//
 // Funciones relacionadas con el SONIDO (mute y control volumen)
 // funcion mute
 let mute = () => {
@@ -86,16 +112,62 @@ window.setVolume = function (val) {
   vid.volume = val / 100;
   console.log("Nivell so: " + vid.volume);
 };
+//*******************************fin SONIDO *************
+//
+//
 
-// funcion para reordenar los videos del array de forma aleatoria
-function reordenar() {
-  for (video of videos) {
-    do {
-      var azar = Math.floor(Math.random() * videos.length);
-    } while (ordenDeLosVideos.indexOf(azar) >= 0);
-    ordenDeLosVideos.push(azar);
+// funcion velocidad de reproduccion ** FALTA
+
+// funcion expandir / contraer video
+let expandir = () => {
+  let sect = document.querySelector("section");
+  // Accedim a transform (propietat CSS)
+  if (sect.style.transform == "scale(0.6)") {
+    // si esta en 0.6 la canviem a 1 i modifiquem icona
+    sect.style.transform = "scale(1)";
+    document.querySelector(".expandir").src = "./images/reducir.svg";
+  } else {
+    sect.style.transform = "scale(0.6)";
+    document.querySelector(".expandir").src = "./images/expandir.svg";
   }
-  return ordenDeLosVideos;
-}
+};
 
-// funcion velocidad de reproduccion
+// funciones que afectan a la BARRA DE PROGRESO
+let actualizarTemps = () => {
+  // mostramos tiempo y durada del video
+  document.querySelector(".estado").innerHTML = `${conversion(
+    vid.currentTime
+  )} / ${conversion(vid.duration)}`;
+  // actualizamos barra de progreso
+  // regla de tres:
+  // si 100% - vid.duration
+  //  x %    - vid.currentTime
+  // entonces :
+  let porcentaje = (100 * vid.currentTime) / vid.duration;
+  // Ahora, con el valor del porcentaje, modificamos el ancho de la
+  // barra roja dinamicamente.
+  document.querySelector(".barra2").style.width = `${porcentaje}%`;
+};
+
+// per parametres pasem els milisegons de la data actual i la duració del video
+let conversion = (segundos) => {
+  let progresoVideo = new Date(segundos * 1000);
+  // si el numero de segons es menor que nou, concatenem un 0 per simular 2 digits
+  let segundo =
+    progresoVideo.getSeconds() <= 9
+      ? "0" + progresoVideo.getSeconds()
+      : progresoVideo.getSeconds();
+  // si el numero de minuts es menor que nou, concatenem un 0 per simular 2 digits
+  let minuto =
+    progresoVideo.getMinutes() <= 9
+      ? "0" + progresoVideo.getMinutes()
+      : progresoVideo.getMinutes();
+  // retornem la conversio milisegons en minuts i segons
+  return `${minuto}:${segundo}`;
+};
+
+// interactuar amb algun punt de la barra de progres
+let buscar = (e) => {
+  let dondeHeHechoClick = e.offsetX;
+  console.log(dondeHeHechoClick);
+};
